5b976b840d988372c06602e9ee13c58780343182
Peiyj <yjpei@ucsd.edu>
2020-02-29 19:24:01 -0800

    1) Added first person camera and movement 2) added gitignore

--- PA4/Window.h
+++ PA4/Window.h
@@ -55,7 +55,8 @@ public:
 	static void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
 	static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos);
 	static glm::vec3 trackBallMapping(double xpos, double ypos);
-	static void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+//	static void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+    static void processInput(GLFWwindow *window);
 };
 
 #endif
